da0be7718cac1f80a97e7f55a16a2698b5b05e27
Vinod <vinod@confianzit.biz>
2021-02-08 11:03:25 +0000

    080221

--- delivery_extension/__manifest__.py
+++ delivery_extension/__manifest__.py
@@ -14,7 +14,9 @@
     'data': [
             "views/stock_view.xml",
             "views/res_partner_view.xml",
-            "views/sale_order_view.xml"
+            "views/sale_order_view.xml",
+            'reports/report_location_barcode.xml'
+
     ],
     'demo_xml': [],
     'installable': True,
