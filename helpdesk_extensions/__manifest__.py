4a7d07e7e73ab6ccd7548f38c31802faba1680fa
Vinod <vinod@confianzit.biz>
2021-08-25 09:18:27 +0000

    help desk

---
+++ helpdesk_extensions/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Help desk extensions',
+    'category': 'helpdesk',
+    'summary': 'Help desk related extensions',
+    'version': '1.0',
+    'description': """
+Help desk extensions
+=============
+
+
+This Module provides the some extra addons on odoo Help Desk.
+
+        """,
+    'author': 'Confianz Global',
+    'depends': ['helpdesk'],
+    'demo': [],
+    'data': [
+
+    ],
+    'qweb': [],
+    'installable': True,
+}
